a0c16c6195211c95905ee800c7625ec00d5a77c9
Nandini <nandini.manthena@gmail.com>
2021-05-27 19:44:15 +0530

    Added echo statement with colors

--- printExample.sh
+++ printExample.sh
@@ -2,3 +2,19 @@
 
 echo Hello People
 
+ ## echo to print multiple lines
+ echo -e "statement1\n , statement2"
+
+ # echo to print colors
+ # syntax: echo -e "\e[COLORCODEmMessage
+
+ # COLORS        CODE
+# RED           31
+# GREEN         32
+# YELLOW        33
+# BLUE          34
+# MAGENTA       35
+# CYAN          36
+
+ echo -e "\e[31mHello In Red Color\e[0m"
+
