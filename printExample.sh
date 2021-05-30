dc35727bc3f8340ce6e0ff6bfd564c82bea58c86
Nandini <nandini.manthena@gmail.com>
2021-05-30 12:58:00 +0530

    Added echo statement

--- printExample.sh
+++ printExample.sh
@@ -5,5 +5,18 @@ echo Hello People
 ## echo to print multiple lines
 echo -e "Hello,\nWelcome to DevOps training"
 
+## Print Text in Colors
+# syntax: echo -e "\e[COLORCODEmMessage"
 
-echo -e "\e[31mHello In Red Color\e[0m"
+# COLORS        CODE
+# RED           31
+# GREEN         32
+# YELLOW        33
+# BLUE          34
+# MAGENTA       35
+# CYAN          36
+
+# Syntax: echo -e "\e[CODEmMessage\e[0m"
+# 0 is color disable
+
+echo -e "\e[37mHello In Red Color\e[0m"
