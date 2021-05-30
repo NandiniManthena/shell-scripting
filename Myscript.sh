b1915ad19f8debe25bb065608fc6e62dbc4ce7ed
Nandini <nandini.manthena@gmail.com>
2021-05-30 12:55:24 +0530

    Added echo statement

--- Myscript.sh
+++ Myscript.sh
@@ -7,5 +7,5 @@ NAME="nandini"
 echo "My Name is $NAME"
 
 #userINPUT
-read-p "enter you name :" NAME
+read -p "enter you name :NAME"
 echo "Hello $NAME , Nice to Meet YOU"
--- printExample.sh
+++ printExample.sh
@@ -5,3 +5,5 @@ echo Hello People
 ## echo to print multiple lines
 echo -e "Hello,\nWelcome to DevOps training"
 
+
+echo -e "\e[31mHello In Red Color\e[0m"
