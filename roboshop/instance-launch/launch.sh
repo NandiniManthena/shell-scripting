d9918ef80bb2b88720bf324ac7362f35914a3e39
Nandini <nandini.manthena@gmail.com>
2021-06-02 18:10:27 +0530

    Added frontend

--- roboshop/instance-launch/launch.sh
+++ roboshop/instance-launch/launch.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 COMPONENT=$1
 
-## =z validates the variable empty , true if its empty
+## -z validates the variable empty , true if it is empty.
 if [ -z "${COMPONENT}" ]; then
-  echo "component input is needed"
+  echo "Component Input is Needed"
   exit 1
 fi
 
-LID=lt-01a6a766d909e9e29
+LID=lt-090f6832097248547
 LVER=1
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=COMPONENT}]" |jq
+## Validate If Instance is already there
 
+INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].State.Name | xargs -n1)
+if [ "${INSTANCE_STATE}" = "running" ]; then
+  echo "Instance already exists!!"
+  exit 0
+fi
+
+if [ "${INSTANCE_STATE}" = "stopped" ]; then
+  echo "Instance already exists!!"
+  exit 0
+fi
+
+aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+© 2021 GitHub, Inc.
