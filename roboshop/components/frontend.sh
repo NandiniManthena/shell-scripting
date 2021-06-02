bccce0952dd7bb459bf8c08aa34f030a86cbcf26
Nandini <nandini.manthena@gmail.com>
2021-06-02 11:53:10 +0530

    Added frontend

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -7,6 +7,11 @@ HEAD "Installing Nginx...."
 yum install nginx -y &>>/tmp/roboshop.log
 STAT $?
 
+HEAD "Start Nginx"
+systemctl restart nginx &>>/tmp/roboshop.log
+systemctl enable nginx &>>/tmp/roboshop.log
+STAT $?
+
 
 
 
