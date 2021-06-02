8f13ae77b334be5c0e42bdc2724bfd96c63a01b6
Nandini <nandini.manthena@gmail.com>
2021-06-02 16:09:34 +0530

    Added frontend

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -3,30 +3,29 @@
 source components/common.sh
 rm -f /tmp/roboshop.log
 
-HEAD "Installing Nginx...."
+HEAD "Installing Nginx....\t"
 yum install nginx -y &>>/tmp/roboshop.log
 STAT $?
 
-
-HEAD "Download from Github"
+HEAD "Download from Github\t"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Delete old HTML DOCS"
+HEAD "Delete old HTML DOCS\t"
 rm -rf /usr/share/nginx/html/*
 STAT $?
 
-HEAD "Extract download content"
+HEAD "Extract download content\t"
 unzip -d /usr/share/nginx/html /tmp/frontend.zip &>>/tmp/roboshop.log
 mv /usr/share/nginx/html/frontend-main/* /usr/share/nginx/html/. &>>/tmp/roboshop.log
 mv /usr/share/nginx/html/static/* /usr/share/nginx/html/. &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Update Nginx Configuration"
+HEAD "Update Nginx Configuration\t"
 mv /usr/share/nginx/html/localhost.conf /etc/nginx/default.d/roboshop.conf
 STAT $?
 
-HEAD "Start Nginx"
+HEAD "Start Nginx\t"
 systemctl restart nginx &>>/tmp/roboshop.log
 systemctl enable nginx &>>/tmp/roboshop.log
 STAT $?
