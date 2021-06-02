c51742a199a8ac650b4b1029ad4b703bc485894e
Nandini <nandini.manthena@gmail.com>
2021-06-02 10:40:46 +0530

    Added makefile

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -10,7 +10,7 @@ echo -e "\e[1m....................................."
 echo -e "\e[1;32mSuccess\t"
 echo -e "....................................\e[0m"
 
-echo -n Installing Nginx...."
+echo -n "Installing Nginx...."
 yum install nginx -y &>>/tmp/roboshop.log
 echo -e "\e[32mdone\e[0m"
 
