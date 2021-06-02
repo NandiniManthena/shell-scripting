29dcc5f44cae1661f314ad8687b83e965834c7d9
Nandini <nandini.manthena@gmail.com>
2021-06-02 12:04:12 +0530

    Added frontend

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -12,6 +12,18 @@ systemctl restart nginx &>>/tmp/roboshop.log
 systemctl enable nginx &>>/tmp/roboshop.log
 STAT $?
 
+HEAD "Download from Github"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Delete old HTML DOCS"
+rm -rf /usr/share/nginx/html/*
+STAT $?
+
+
+
+
+
 
 
 
