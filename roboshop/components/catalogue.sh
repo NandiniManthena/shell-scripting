a13e6d6d44dd80e4863f1e0b09596cd8ae591ac3
Nandini <nandini.manthena@gmail.com>
2021-06-04 20:13:51 +0530

    Added frontend

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-echo Installing catalogue
+source components/common.sh
+rm -f /tmp/roboshop.log
+
+HEAD "Installing NodeJS"
+ yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
+ STAT $?
+
+HEAD "ADD Roboshop App USer"
+useradd roboshop &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Download curl from Git"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"&>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Extract the downloaded archive"
+cd /home/roboshop && unzip /tmp/catalogue.zip &>>/tmp/roboshop.log && mv catalogue-main catalogue
+STAT $?
+
+HEAD "Install nodejs dependencies"
+cd /home/roboshop/catalogue && npm install &>>/tmp/roboshop.log
+STAT $?
+
+
+
