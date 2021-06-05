fab05854e807112edf09351ce9c961eaea3368cc
Nandini <nandini.manthena@gmail.com>
2021-06-05 17:28:56 +0530

    Added frontend

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo Installing redis
+source components/common.sh
+rm -f /tmp/roboshop.log
+
+HEAD "set up Redis Repos"
+ yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>/tmp/roboshop.log && yum-config-manager --enable remi &>>/tmp/roboshop.log
+ STAT $?
+# yum install redis -y
