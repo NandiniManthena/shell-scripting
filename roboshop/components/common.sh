4e7b7ce53b93d9a20e76de210df8c3543bb5bc6e
Nandini <nandini.manthena@gmail.com>
2021-06-05 18:55:12 +0530

    Added frontend

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -24,6 +24,16 @@ else
 fi
 }
 
+SETUP_SYSTEMD() {
+  HEAD "Setup SystemD Service\t\t"
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' /home/roboshop/$1/systemd.service  && mv /home/roboshop/$1/systemd.service /etc/systemd/system/$1.service
+  STAT $?
+
+  HEAD "Start $1 Service\t"
+  systemctl daemon-reload && systemctl enable $1 &>>/tmp/roboshop.log && systemctl restart $1 &>>/tmp/roboshop.log
+  STAT $?
+}
+
 NODEJS(){
   HEAD "Installing NodeJS"
  yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
@@ -47,12 +57,6 @@ HEAD "'Fix Permissions to App content"
 chown roboshop:roboshop /home/roboshop -R
 STAT $?
 
-HEAD "setup SYSTEMD service"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/$1/systemd.service && mv /home/roboshop/$1/systemd.service /etc/systemd/system/$1.service
-STAT $?
-
-HEAD "Start $1 service"
-systemctl daemon-reload && systemctl enable $1 &>>/tmp/roboshop.log && systemctl restart $1 &>>/tmp/roboshop.log
-STAT $?
+SETUP_SYSTEMD '$1'
 
 }
