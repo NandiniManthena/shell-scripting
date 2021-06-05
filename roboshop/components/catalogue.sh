6fef597f369722843a913ef288559075696ce3d8
Nandini <nandini.manthena@gmail.com>
2021-06-05 18:43:00 +0530

    Added frontend

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -2,45 +2,9 @@
 
 source components/common.sh
 rm -f /tmp/roboshop.log
+set-hostname catalogue
 
-HEAD "Installing NodeJS"
- yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
- STAT $?
-
-HEAD "ADD Roboshop App USer"
-id roboshop &>>/tmp/roboshop.log
-if [ $? -eq 0 ]; then
-  echo user is already thereso skippion &>>/tmp/roboshop.log
-  STAT $?
-else
-  useradd roboshop &>>/tmp/roboshop.log
-  STAT $?
-fi
-
-HEAD "Download curl from Git"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"&>>/tmp/roboshop.log
-STAT $?
-
-HEAD "Extract the downloaded archive"
-cd /home/roboshop && rm -rf catalogue && unzip /tmp/catalogue.zip &>>/tmp/roboshop.log && mv catalogue-main catalogue
-STAT $?
-
-HEAD "Install nodejs dependencies"
-cd /home/roboshop/catalogue && npm install --unsafe-perm &>>/tmp/roboshop.log
-STAT $?
-
-HEAD "'Fix Permissions to App content"
-chown roboshop:roboshop /home/roboshop -R
-STAT $?
-
-HEAD "setup SYSTEMD service"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-STAT $?
-
-HEAD "Start catalogue service"
-systemctl daemon-reload && systemctl enable catalogue &>>/tmp/roboshop.log && systemctl restart catalogue &>>/tmp/roboshop.log
-STAT $?
-
+NODEJS "catalogue"
 
 
 
--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -11,3 +11,48 @@ STAT(){
   exit 1
   fi
 }
+
+APP_USER_ADD(){
+  HEAD "ADD Roboshop App USer"
+id roboshop &>>/tmp/roboshop.log
+if [ $? -eq 0 ]; then
+  echo user is already thereso skippion &>>/tmp/roboshop.log
+  STAT $?
+else
+  useradd roboshop &>>/tmp/roboshop.log
+  STAT $?
+fi
+}
+
+NODEJS(){
+  HEAD "Installing NodeJS"
+ yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
+ STAT $?
+
+APP_USER_ADD
+
+HEAD "Download curl from Git"
+curl -s -L -o /tmp/$1.zip "https://github.com/roboshop-devops-project/$1/archive/main.zip"&>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Extract the downloaded archive"
+cd /home/roboshop && rm -rf $1 && unzip /tmp/$1.zip &>>/tmp/roboshop.log && mv $1-main $1
+STAT $?
+
+HEAD "Install nodejs dependencies"
+cd /home/roboshop/$1 && npm install --unsafe-perm &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "'Fix Permissions to App content"
+chown roboshop:roboshop /home/roboshop -R
+STAT $?
+
+HEAD "setup SYSTEMD service"
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/$1/systemd.service && mv /home/roboshop/$1/systemd.service /etc/systemd/system/$1.service
+STAT $?
+
+HEAD "Start $1 service"
+systemctl daemon-reload && systemctl enable $1 &>>/tmp/roboshop.log && systemctl restart $1 &>>/tmp/roboshop.log
+STAT $?
+
+}
