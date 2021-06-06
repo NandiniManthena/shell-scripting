93ca47a300ec9562ac01d51c5dc8159887a36d5a
Nandini <nandini.manthena@gmail.com>
2021-06-06 16:09:17 +0530

    Added frontend

--- roboshop/instance-launch/launch.sh
+++ roboshop/instance-launch/launch.sh
@@ -16,7 +16,7 @@ LVER=1
 DNS_UPDATE() {
   PRIVATEIP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].PrivateIpAddress | xargs -n1)
   sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z04221902CM9ZT2GHM1NW --change-batch file:///tmp/record.json | jq
+  aws route53 change-resource-record-sets --hosted-zone-id Z036612119XLJGCED4L2T --change-batch file:///tmp/record.json | jq
 }
 
 INSTANCE_CREATE() {
