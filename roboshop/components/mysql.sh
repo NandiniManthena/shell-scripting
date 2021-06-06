4319492f2e0102c7789b3b7a4f978432c5c24649
Nandini <nandini.manthena@gmail.com>
2021-06-06 18:37:47 +0530

    Added frontend

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-echo Installing mysql
+source components/common.sh
+rm -f /tmp/roboshop.log
+set-hostname mysql
+
+HEAD "setup mysql repo"
+echo '[mysql57-community]
+name=MySQL 5.7 Community Server
+baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
+enabled=1
+gpgcheck=0' > /etc/yum.repos.d/mysql.repo
+STAT $?
+
+HEAD "install mysql server"
+ yum remove mariadb-libs -y &>>/tmp/roboshop.log && yum install mysql-community-server -y &>>/tmp/roboshop.log
+ STAT $?
+
+HEAD "start mysql server"
+systemctl enable mysqld &>>/tmp/roboshop.log && systemctl start mysqld &>>/tmp/roboshop.log
+STAT $?
+
+
